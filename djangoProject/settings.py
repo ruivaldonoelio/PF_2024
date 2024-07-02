24e231f3c4051d771095fb25fa65324613fce351
Ruivaldo Santana <92994350+ruivaldonoelio@users.noreply.github.com>
2024-07-03 00:43:49 +0100

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -145,6 +145,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "static/",
+]
 
 MEDIA_URL = '/media/'  # URL base para servir arquivos de mídia
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
