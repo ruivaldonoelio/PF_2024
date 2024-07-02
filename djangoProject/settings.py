6da42a1d1db29b0f27b581954e11169f6c624553
ruivaldo <30009351@students.ual.pt>
2024-07-02 17:34:12 +0100

    So limar nos erros que podem aparecer com o tempo

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -16,6 +16,7 @@ import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 import dj_database_url
+import sys
 
 # Set the project base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
