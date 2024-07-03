3c284d2bde227d532f08d78fe83b61c5f56aa925
ruivaldo <30009351@students.ual.pt>
2024-07-03 01:30:09 +0100

    So limar nos erros que podem aparecer com o tempo

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'  # URL base para servir arquivos de mídia
